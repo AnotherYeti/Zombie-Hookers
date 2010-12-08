626fe892cfb4320c64cb45246f42ccea791136f3
Connor Janowiak <connor.janowiak@gmail.com>
2010-12-08 16:54:39 -0700

    Added basic Twitter2Image

--- touchyfeely/src/main/java/net/zombiehookers/ViewServlet.java
+++ touchyfeely/src/main/java/net/zombiehookers/ViewServlet.java
@@ -9,7 +9,6 @@ import java.io.IOException;
 public class ViewServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.setAttribute("contacts", AddressBook.getContacts());
         req.getRequestDispatcher("/WEB-INF/jsp/view.jsp").forward(req, resp);
     }
 }
