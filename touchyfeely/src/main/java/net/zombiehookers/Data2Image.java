b3a418fbcd15706a8276854517b192cf8b10df83
Aleksasndr Rudenko <aleksasndr.rudenko@colorado.edu>
2010-12-09 14:11:40 -0700

    made the import

--- touchyfeely/src/main/java/net/zombiehookers/Data2Image.java
+++ touchyfeely/src/main/java/net/zombiehookers/Data2Image.java
@@ -6,7 +6,7 @@ import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-//import Twitter2Image.java;
+import net.zombiehookers.Twitter2Image;
 
 public class Data2Image
 {
