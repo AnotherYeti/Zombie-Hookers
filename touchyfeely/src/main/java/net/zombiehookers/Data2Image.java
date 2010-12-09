a8772672bbaa8778ff3a0cab10197cac0f0da709
Aleksasndr Rudenko <aleksasndr.rudenko@colorado.edu>
2010-12-09 13:49:18 -0700

    separate file for mine

---
+++ touchyfeely/src/main/java/net/zombiehookers/Data2Image.java
@@ -0,0 +1,58 @@
+package net.zombiehookers;
+
+import java.util.List;
+import javax.imageio.ImageIO;
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+
+public class Data2Image
+{
+
+	File filename = new File("mood_overlay.jpg");
+	private BufferedImage image;
+	private double array[][];
+	private Color clear = new Color(0,0,0,0);
+
+	private int w = 250;
+	private int h = 600;
+	
+	public void set(int i, int j, Color c)
+	{
+		image.setRGB(i, j, c.getRGB());
+	}
+	
+	public void Process()
+	{
+        	image = new BufferedImage(250, 600, BufferedImage.TYPE_INT_ARGB);
+		for (int a = 0; a < w-1; a ++)
+		{
+			for (int b = 0; b < h-1; b ++)
+			{
+				image.setRGB(a, b, clear.getRGB());
+			}
+		}	
+		for (int a = 0; a < array.length-1; a ++)
+		{
+			double mood = array[a][0];
+			double X = array[a][1];
+			double Y = array[a][2];
+			int temp_X = (int)(10*(X-25));
+			int temp_Y = (int)(10*(Y+125));
+			if (mood == 0){set(temp_X, temp_Y, Color.blue);}
+			else{set(temp_X, temp_Y, Color.red);}
+		}
+		try {
+			ImageIO.write(image,"jpg",filename);
+		} catch ( IOException e) {
+		}
+	}
+
+	//public static void main(String[] args)
+	//{
+	//	Picture pic = new Picture(250, 600);
+	//	ImageIO.write(image,"jpg",filename);
+	//}
+}
