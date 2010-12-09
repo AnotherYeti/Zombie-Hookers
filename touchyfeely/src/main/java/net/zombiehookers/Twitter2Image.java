e33c20bef090d543846eee1887f668977b2de680
Connor Janowiak <connor.janowiak@gmail.com>
2010-12-09 14:12:14 -0700

    Added emoxels

--- touchyfeely/src/main/java/net/zombiehookers/Twitter2Image.java
+++ touchyfeely/src/main/java/net/zombiehookers/Twitter2Image.java
@@ -9,10 +9,13 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
+public class Emoxel {
+	public double lat,lng,e;
+}
 
 public class Twitter2Image {
 	
-	private List<Tweet> tweets;
+	private List<Emoxel> emoxels;
 	private TwitterFactory tf;
 	private Twitter t;
 
@@ -21,19 +24,27 @@ public class Twitter2Image {
 		t = tf.getInstance();
 	}
 
-	public List<Tweet> getTweets() {
-		return tweets;
+	public List<Emoxel> getTweets() {
+		return emoxels;
 	}
 
 	public void updateTweets(double lat, double lng, double radius) {
 		GeoLocation g = new GeoLocation(lat, lng);
 		Query query = new Query();
 		query.setGeoCode(g, radius, Query.MILES);
+		List<Tweet> tweets;
 	
 		try {	
 			tweets = t.search(query).getTweets();
 		} catch (TwitterException e) {
 		}
-	}
 
+		for(i = 0; i < tweets.size(); i++)
+		{
+			Emoxel e = new Emoxel();
+			e.lat = tweets.get(i).getGeolocation().getLatitude();
+			e.lng = tweets.get(i).getGeolocation().getLongitude();
+			e.e = 1.0f;
+		}
+	}
 }
