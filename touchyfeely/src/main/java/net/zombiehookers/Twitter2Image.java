c4f5633a3d3ae2160b55cb495146a5aa71475dff
Aleksasndr Rudenko <aleksasndr.rudenko@colorado.edu>
2010-12-09 13:53:14 -0700

    spit two files

--- touchyfeely/src/main/java/net/zombiehookers/Twitter2Image.java
+++ touchyfeely/src/main/java/net/zombiehookers/Twitter2Image.java
@@ -35,49 +35,5 @@ public class Twitter2Image {
 		} catch (TwitterException e) {
 		}
 	}
-	File filename = new File("mood_overlay.jpg");
-	private BufferedImage image;
-	private double array[][];
-	private Color clear = new Color(0,0,0,0);
-
-	private int w = 250;
-	private int h = 600;
-	
-	public void set(int i, int j, Color c)
-	{
-		image.setRGB(i, j, c.getRGB());
-	}
-	
-	public void Process()
-	{
-        	image = new BufferedImage(250, 600, BufferedImage.TYPE_INT_ARGB);
-		for (int a = 0; a < w-1; a ++)
-		{
-			for (int b = 0; b < h-1; b ++)
-			{
-				image.setRGB(a, b, clear.getRGB());
-			}
-		}	
-		for (int a = 0; a < array.length-1; a ++)
-		{
-			double mood = array[a][0];
-			double X = array[a][1];
-			double Y = array[a][2];
-			int temp_X = (int)(10*(X-25));
-			int temp_Y = (int)(10*(Y+125));
-			if (mood == 0){set(temp_X, temp_Y, Color.blue);}
-			else{set(temp_X, temp_Y, Color.red);}
-		}
-		try {
-			ImageIO.write(image,"jpg",filename);
-		} catch ( IOException e) {
-		}
-	}
-
-	//public static void main(String[] args)
-	//{
-	//	Picture pic = new Picture(250, 600);
-	//	ImageIO.write(image,"jpg",filename);
-	//}
 
 }
